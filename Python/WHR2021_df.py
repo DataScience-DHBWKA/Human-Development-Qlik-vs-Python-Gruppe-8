f503a423e7a65985e4f62116477b721c6ade9f07
Phiphi67 <philippe30002@gmail.com>
2024-05-04 16:56:31 +0200

    added WHR2021_df.py

---
+++ Python/WHR2021_df.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from Python_param import folder_path
+
+WHR2021_df = pd.DataFrame()
+WHR2021_df = pd.read_csv(folder_path + '/WHR2021.csv')
+WHR2021_df = WHR2021_df[['Country name', 'Ladder score']]
+WHR2021_df.rename(columns={'Country name': 'Country or Area'}, inplace=True)
+WHR2021_df.rename(columns={'Ladder Score': 'Value'}, inplace=True)
