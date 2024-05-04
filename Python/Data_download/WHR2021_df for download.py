782a2cac0f91e8df27732c56e4345c03a0ed55e5
Phiphi67 <philippe30002@gmail.com>
2024-05-04 18:54:55 +0200

    added WHR2021_df for download.py

---
+++ Python/Data_download/WHR2021_df for download.py
@@ -0,0 +1,10 @@
+import sys
+import os
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(parent_dir)
+from Data_method.WHR2021_df import WHR2021_df
+from Python_param import output_file_path_WHR2021_df as output_file_path
+
+#Let's go export with csv
+WHR2021_df.to_csv(output_file_path, index=False)
+print(f"Filtered data saved to {output_file_path}")
--- Python/python_param.py
+++ Python/python_param.py
@@ -1,5 +1,6 @@
 output_file_path_filtered_data = 'filtered_data.csv'
 output_file_path_suicide_df = 'suicide_df.csv'
 output_file_path_Natural_disasters_df = 'Natural_disasters_df.csv'
+output_file_path_WHR2021_df = 'WHR2021_df.csv'
 folder_path = 'data'
 folder_path_2 = 'data'
