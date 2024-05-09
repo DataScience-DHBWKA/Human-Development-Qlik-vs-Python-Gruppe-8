675a57ed26b936454cdde587cd1f9912d94723e1
Phiphi67 <philippe30002@gmail.com>
2024-05-09 23:05:28 +0200

    add some comments in data_download

--- Python/Data_download/Filtered_data for download .py
+++ Python/Data_download/Filtered_data for download .py
@@ -1,10 +1,12 @@
 import sys
 import os
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.append(parent_dir)
+
+# Importing necessary modules
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) # Getting the parent directory path
+sys.path.append(parent_dir) # Appending parent directory to system path for module import
 from Filtered_data import result_df
 from python_param import output_file_path_filtered_data as output_file_path
 
-#Let's go export with csv
+# Exporting data to CSV file
 result_df.to_csv(output_file_path, index=False)
 print(f"Filtered data saved to {output_file_path}")
--- Python/Data_download/WHR2021_df for download.py
+++ Python/Data_download/WHR2021_df for download.py
@@ -1,10 +1,12 @@
 import sys
 import os
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.append(parent_dir)
+
+# Importing necessary modules
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) # Getting the parent directory path
+sys.path.append(parent_dir) # Appending parent directory to system path for module import
 from Data_method.WHR2021_df import WHR2021_df
 from python_param import output_file_path_WHR2021_df as output_file_path
 
-#Let's go export with csv
+# Exporting data to CSV file
 WHR2021_df.to_csv(output_file_path, index=False)
 print(f"Filtered data saved to {output_file_path}")
